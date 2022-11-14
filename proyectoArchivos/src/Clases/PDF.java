a4ea696b9e9dc837d02a487f55e421a18ccfd323
danieltistoj <jose.tisrey@gmail.com>
2022-11-14 13:41:55 -0600

    creacion de funcion getVersiion

--- proyectoArchivos/src/Clases/PDF.java
+++ proyectoArchivos/src/Clases/PDF.java
@@ -6,17 +6,35 @@
 package Clases;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Usuario
  */
 public class PDF {
+      private String version;
+      private File archivo;
     
      PDF(File archivo){
-        
+        this.archivo = archivo;
     }
-    public String getVersion(){
-        return "";
+    public String getVersion(){ 
+          try {
+              RandomAccessFile newarchivo = new RandomAccessFile(archivo,"r");
+              this.version=newarchivo.readLine();
+              newarchivo.close();
+             
+              return this.version;
+          } catch (FileNotFoundException ex) {
+              Logger.getLogger(PDF.class.getName()).log(Level.SEVERE, null, ex);
+          }catch (IOException ex) {
+            Logger.getLogger(PDF.class.getName()).log(Level.SEVERE, null, ex);
+        }
+          return this.version;
     }
 }
